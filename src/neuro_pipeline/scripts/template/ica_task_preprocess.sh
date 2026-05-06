d43e5267d898b1ae0d0ca74d603f6df8a55b114c
Qiuyu <qiuyuyu.psych@gmail.com>
2026-05-06 01:56:32 -0400

    Refactor ICA task preprocessing script for clarity and improved output handling

--- src/neuro_pipeline/scripts/template/ica_task_preprocess.sh
+++ src/neuro_pipeline/scripts/template/ica_task_preprocess.sh
@@ -3,7 +3,7 @@
 # ---------------------------------- Setup ---------------------------------------------
 
 subject="$1"
-echo "Processing subject: $subject for BIDS reconstruction"
+echo "Processing subject: $subject for ICA task preprocessing"
 
 # Use the global variables
 input_dir="$INPUT_DIR"
@@ -25,36 +25,25 @@ echo "remove trs: ${REMOVE_TRS}"
 echo "smoothing or blur size: ${BLUR_SIZE}"
 echo "censor_motion: ${CENSOR_MOTION}"
 echo "censor_outliers: ${CENSOR_OUTLIERS}"
+echo "bandpass: ${BANDPASS_LOW} - ${BANDPASS_HIGH}"
 
 # ---------------------------------- Run Processing -------------------------------------
-echo "stimulus directory: ${STIMULUS_DIR}"
-stimulus_dir="${STIMULUS_DIR}/kidvid_stim_timing_files"
 
 subj="${prefix}${subject}"
 t1_dir="${output_dir}/${subj}/ses-${session}/sswarp2"
 nifti_dir="${input_dir}/sub-${subject}/ses-${session}/func"
 
-kidvid_output_dir="${output_dir}/${subj}/ses-${session}/kidvid_output"
-mkdir -p "${kidvid_output_dir}"
+ica_task_output_dir="${output_dir}/${subj}/ses-${session}/ica_task_output"
+mkdir -p "${ica_task_output_dir}"
 
-cd "${kidvid_output_dir}"
-
-# env > env.sbatch.log
+cd "${ica_task_output_dir}"
 
 echo "find "${nifti_dir}"/sub-"${subject}"_ses-"${session}"_task-kidvid*.nii*"
 
 if ! ls "${nifti_dir}"/sub-"${subject}"_ses-"${session}"_task-kidvid*.nii* 1> /dev/null 2>&1; then
-    echo "Not found nii file or maybe the name is wrong in $data_dir"
+    echo "Not found nii file or maybe the name is wrong in ${nifti_dir}"
     exit 1
 fi
-
-if ls "${nifti_dir}"/sub-"${subject}"_ses-"${session}"_task-kidvidB*.nii* 1> /dev/null 2>&1; then
-    version="B"
-else
-    version="A"
-fi
-
-echo "version: ${version}"
 
 export AFNI_NO_X11=1
 
@@ -65,7 +54,7 @@ afni_proc.py \
 -copy_anat "${t1_dir}"/T1_results/anatSS."${subj}".nii \
 -anat_has_skull no \
 -dsets "${nifti_dir}"/sub-"${subject}"_ses-"${session}"_task-kidvid*.nii* \
--blocks tshift align tlrc volreg blur mask scale regress \
+-blocks despike tshift align tlrc volreg blur mask regress \
 -radial_correlate_blocks tcat volreg regress \
 -tcat_remove_first_trs "${REMOVE_TRS}" \
 -align_unifize_epi local \
@@ -81,31 +70,12 @@ afni_proc.py \
 -volreg_tlrc_warp \
 -volreg_compute_tsnr yes \
 -mask_epi_anat yes \
--test_stim_files no \
 -blur_size "${BLUR_SIZE}" \
--regress_stim_times \
-	"${stimulus_dir}"/${version}_remove_6TRs/negative.1D \
-	"${stimulus_dir}"/${version}_remove_6TRs/neutral.1D \
-	"${stimulus_dir}"/${version}_remove_6TRs/positive.1D \
--regress_stim_labels neg neut pos \
--regress_basis_multi 'dmBLOCK' 'dmBLOCK' 'dmBLOCK'  \
--regress_stim_types AM1 AM1 AM1 \
--regress_opts_3dD \
--gltsym 'SYM: pos -neut' \
--gltsym 'SYM: neg -neut' \
--gltsym 'SYM: pos -neg' \
--gltsym 'SYM: 0.5*pos +0.5*neg -neut' \
--glt_label 1 Pos-Neut \
--glt_label 2 Neg-Neut \
--glt_label 3 Pos-Neg \
--glt_label 4 PosNeg-Neut \
+-regress_apply_mot_types demean deriv \
+-regress_motion_per_run \
 -regress_censor_motion "${CENSOR_MOTION}" \
 -regress_censor_outliers "${CENSOR_OUTLIERS}" \
--regress_motion_per_run \
--regress_3dD_stop \
--regress_reml_exec \
--regress_compute_fitts \
--regress_make_ideal_sum sum_ideal.1D \
+-regress_bandpass "${BANDPASS_LOW}" "${BANDPASS_HIGH}" \
 -regress_est_blur_epits \
 -regress_est_blur_errts \
 -regress_run_clustsim no \
